9040e4db21469d2866c9fcffa40e40865826b4b0
Marin <marinmema@live.ca>
2025-11-12 08:55:17 -0500

    use app.listen(), prettier

--- index.ts
+++ index.ts
@@ -4,48 +4,51 @@
  * The webpages served from ./public use @simplewebauthn/browser.
  */
 
-import http from 'http';
-import express from 'express';
-import session from 'express-session';
-import memoryStore from 'memorystore';
-import dotenv from 'dotenv';
+import express from "express";
+import session from "express-session";
+import memoryStore from "memorystore";
+import dotenv from "dotenv";
 
 dotenv.config();
 
-import { expectedOrigin, host, port, rpID } from './server/constants';
-import { GenerateRegistrationOptionsController } from './server/controllers/GenerateRegistrationOptionsController';
-import { VerifyRegistrationController } from './server/controllers/VerifyRegistrationController';
-import { GenerateAuthenticationOptionsController } from './server/controllers/GenerateAuthenticationOptions';
-import { VerifyAuthenticationController } from './server/controllers/VerifyAuthenticationController';
+import { host, port } from "./server/constants";
+import { GenerateRegistrationOptionsController } from "./server/controllers/GenerateRegistrationOptionsController";
+import { VerifyRegistrationController } from "./server/controllers/VerifyRegistrationController";
+import { GenerateAuthenticationOptionsController } from "./server/controllers/GenerateAuthenticationOptions";
+import { VerifyAuthenticationController } from "./server/controllers/VerifyAuthenticationController";
 
 const app = express();
 const MemoryStore = memoryStore(session);
 
-app.use(express.static('./public/'));
+app.use(express.static("./public/"));
 app.use(express.json());
 app.use(
-  session({
-    secret: 'secret123',
-    saveUninitialized: true,
-    resave: false,
-    cookie: {
-      maxAge: 86400000,
-      httpOnly: true, // Ensure to not expose session cookies to clientside scripts
-    },
-    store: new MemoryStore({
-      checkPeriod: 86_400_000, // prune expired entries every 24h
-    }),
-  }),
+    session({
+        secret: "secret123",
+        saveUninitialized: true,
+        resave: false,
+        cookie: {
+            maxAge: 86400000,
+            httpOnly: true, // Ensure to not expose session cookies to clientside scripts
+        },
+        store: new MemoryStore({
+            checkPeriod: 86_400_000, // prune expired entries every 24h
+        }),
+    })
 );
 
-app.get('/generate-registration-options', GenerateRegistrationOptionsController);
+app.get(
+    "/generate-registration-options",
+    GenerateRegistrationOptionsController
+);
 
-app.post('/verify-registration', VerifyRegistrationController);
+app.post("/verify-registration", VerifyRegistrationController);
 
-app.get('/generate-authentication-options', GenerateAuthenticationOptionsController);
+app.get(
+    "/generate-authentication-options",
+    GenerateAuthenticationOptionsController
+);
 
-app.post('/verify-authentication', VerifyAuthenticationController);
+app.post("/verify-authentication", VerifyAuthenticationController);
 
-http.createServer(app).listen(port, host, () => {
-  console.log(`🚀 Server ready at ${expectedOrigin} (${host}:${port})`);
-});
+app.listen(port, host, () => console.log(`🚀 Server ready at ${host}:${port}`));
