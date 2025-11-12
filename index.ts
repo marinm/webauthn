e5c212984eb4104c937ee5a980640321d4d153f7
Marin <marinmema@live.ca>
2025-11-12 08:51:55 -0500

    extract VerifyAuthenticationController

--- index.ts
+++ index.ts
@@ -12,19 +12,11 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-import {
-  AuthenticationResponseJSON,
-  VerifiedAuthenticationResponse,
-  verifyAuthenticationResponse,
-  VerifyAuthenticationResponseOpts,
-  WebAuthnCredential,
-} from '@simplewebauthn/server';
-
 import { expectedOrigin, host, port, rpID } from './server/constants';
-import { inMemoryUserDB, loggedInUserId } from './server/in-memory-user-db';
 import { GenerateRegistrationOptionsController } from './server/controllers/GenerateRegistrationOptionsController';
 import { VerifyRegistrationController } from './server/controllers/VerifyRegistrationController';
 import { GenerateAuthenticationOptionsController } from './server/controllers/GenerateAuthenticationOptions';
+import { VerifyAuthenticationController } from './server/controllers/VerifyAuthenticationController';
 
 const app = express();
 const MemoryStore = memoryStore(session);
@@ -52,56 +44,7 @@ app.post('/verify-registration', VerifyRegistrationController);
 
 app.get('/generate-authentication-options', GenerateAuthenticationOptionsController);
 
-app.post('/verify-authentication', async (req, res) => {
-  const body: AuthenticationResponseJSON = req.body;
-
-  const user = inMemoryUserDB[loggedInUserId];
-
-  const expectedChallenge = req.session.currentChallenge;
-
-  let dbCredential: WebAuthnCredential | undefined;
-  // "Query the DB" here for a credential matching `cred.id`
-  for (const cred of user.credentials) {
-    if (cred.id === body.id) {
-      dbCredential = cred;
-      break;
-    }
-  }
-
-  if (!dbCredential) {
-    return res.status(400).send({
-      error: 'Authenticator is not registered with this site',
-    });
-  }
-
-  let verification: VerifiedAuthenticationResponse;
-  try {
-    const opts: VerifyAuthenticationResponseOpts = {
-      response: body,
-      expectedChallenge: `${expectedChallenge}`,
-      expectedOrigin,
-      expectedRPID: rpID,
-      credential: dbCredential,
-      requireUserVerification: false,
-    };
-    verification = await verifyAuthenticationResponse(opts);
-  } catch (error) {
-    const _error = error as Error;
-    console.error(_error);
-    return res.status(400).send({ error: _error.message });
-  }
-
-  const { verified, authenticationInfo } = verification;
-
-  if (verified) {
-    // Update the credential's counter in the DB to the newest count in the authentication
-    dbCredential.counter = authenticationInfo.newCounter;
-  }
-
-  req.session.currentChallenge = undefined;
-
-  res.send({ verified });
-});
+app.post('/verify-authentication', VerifyAuthenticationController);
 
 http.createServer(app).listen(port, host, () => {
   console.log(`🚀 Server ready at ${expectedOrigin} (${host}:${port})`);
---
+++ server/controllers/VerifyAuthenticationController.ts
@@ -0,0 +1,61 @@
+import {
+    AuthenticationResponseJSON,
+    VerifiedAuthenticationResponse,
+    verifyAuthenticationResponse,
+    VerifyAuthenticationResponseOpts,
+    WebAuthnCredential,
+} from "@simplewebauthn/server";
+import { expectedOrigin, rpID } from "../constants";
+import { inMemoryUserDB, loggedInUserId } from "../in-memory-user-db";
+
+export async function VerifyAuthenticationController(req: any, res: any) {
+    const body: AuthenticationResponseJSON = req.body;
+
+    const user = inMemoryUserDB[loggedInUserId];
+
+    const expectedChallenge = req.session.currentChallenge;
+
+    let dbCredential: WebAuthnCredential | undefined;
+    // "Query the DB" here for a credential matching `cred.id`
+    for (const cred of user.credentials) {
+        if (cred.id === body.id) {
+            dbCredential = cred;
+            break;
+        }
+    }
+
+    if (!dbCredential) {
+        return res.status(400).send({
+            error: "Authenticator is not registered with this site",
+        });
+    }
+
+    let verification: VerifiedAuthenticationResponse;
+    try {
+        const opts: VerifyAuthenticationResponseOpts = {
+            response: body,
+            expectedChallenge: `${expectedChallenge}`,
+            expectedOrigin,
+            expectedRPID: rpID,
+            credential: dbCredential,
+            requireUserVerification: false,
+        };
+        verification = await verifyAuthenticationResponse(opts);
+    } catch (error) {
+        const _error = error as Error;
+        console.error(_error);
+        return res.status(400).send({ error: _error.message });
+    }
+
+    const { verified, authenticationInfo } = verification;
+
+    if (verified) {
+        // Update the credential's counter in the DB to the newest count in the
+        // authentication
+        dbCredential.counter = authenticationInfo.newCounter;
+    }
+
+    req.session.currentChallenge = undefined;
+
+    res.send({ verified });
+}
