b392bf79c884c4b9fe6df7b3e06ae8c8fd508d8d
Marin <marinmema@live.ca>
2025-11-17 10:17:07 -0500

    allow any credentials

--- server/controllers/generate-authentication-options-controller.ts
+++ server/controllers/generate-authentication-options-controller.ts
@@ -10,31 +10,15 @@ export async function GenerateAuthenticationOptionsController(
   req: Request,
   res: Response,
 ) {
-  // You need to know the user by this point
-  const user = req.user;
-
   const opts: GenerateAuthenticationOptionsOpts = {
     timeout: 60000,
-    allowCredentials: user.credentials.map((cred: WebAuthnCredential) => ({
-      id: cred.id,
-      type: "public-key",
-      transports: cred.transports,
-    })),
-    /**
-     * Wondering why user verification isn't required? See here:
-     *
-     * https://passkeys.dev/docs/use-cases/bootstrapping/#a-note-about-user-verification
-     */
+    allowCredentials: [],
     userVerification: "preferred",
     rpID,
   };
 
   const options = await generateAuthenticationOptions(opts);
 
-  /**
-   * The server needs to temporarily remember this value for verification, so don't lose it until
-   * after you verify the authentication response.
-   */
   req.session.currentChallenge = options.challenge;
 
   res.send(options);
