0bd2107ed7ed983fc68769e43770fcc95b728212
Marin <marinmema@live.ca>
2025-11-19 09:45:17 -0500

    delete constants and dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,29 @@
-import dotenv from "dotenv";
-
-dotenv.config();
-
 import express from "express";
 import routes from "./routes";
 import session from "./session";
-import { expectedOrigin, host, port, rpID } from "./constants";
+
+/**
+ * If the server is behind a proxy, the internal host and port are different
+ * from the relying party ID and expected origin.
+ */
+const host = "localhost";
+const port = 8005;
+
+/**
+ * RP ID represents the "scope" of websites on which a credential should be
+ * usable. The Origin represents the expected URL from which registration or
+ * authentication occurs.
+ */
+const rpID = "localhost";
+const expectedOrigin = `http://${host}:${port}`;
+
+/**
+ * A standalone SQLite database is created with a passkeys table. The parent
+ * folder should already exist. The file itself will be created if it doesn't
+ * already exist, but the server process should have read and write access to
+ * it.
+ */
+const databaseFilepath = "./passkeys.sqlite";
 
 const app = express();
 
@@ -16,8 +34,10 @@ app.use(
   routes({
     rpID,
     expectedOrigin,
-    databaseFilepath: process.env.DB_FILE_NAME!,
+    databaseFilepath,
   }),
 );
 
-app.listen(port, host, () => console.log(`🚀 Server ready at ${host}:${port}`));
+app.listen(port, host, () =>
+  console.log(`🚀 Server ready at ${expectedOrigin}`),
+);
