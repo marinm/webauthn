ec3841d2ac034f2d09dbafe2fd858f864fa540a9
Marin <marinmema@live.ca>
2025-11-21 07:48:36 -0500

    edit comments

--- src/WebAuthnOptions.ts
+++ src/WebAuthnOptions.ts
@@ -1,14 +1,14 @@
 export type WebAuthnOptions = {
   /**
-   * RP ID represents the "scope" of websites on which a credential should be
-   * usable. The Origin represents the expected URL from which registration or
-   * authentication occurs.
+   * The relying party ID represents the "scope" of websites on which a
+   * credential should be usable.
    */
   rpID: string;
 
   /**
-   * If the server is behind a proxy, the internal host and port are different
-   * from the relying party ID and expected origin.
+   * The origin represents the expected URL from which registration or
+   * authentication occurs. If the server is behind a proxy, the internal host
+   * and port are different from the relying party ID and expected origin.
    */
   expectedOrigin: string;
 
